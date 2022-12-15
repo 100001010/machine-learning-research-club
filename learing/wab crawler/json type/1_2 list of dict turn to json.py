0b44e9eb612024cbc0c5ce66586006943d68b5c1
100001010 <119588517+100001010@users.noreply.github.com>
2022-12-15 17:46:43 +0800

    add wab crawler

---
+++ learing/wab crawler/json type/1_2 list of dict turn to json.py
@@ -0,0 +1,7 @@
+import json
+
+listObj=[{'name':'peter','age':'25','gender':'M'}]
+jsonData=json.dumps(listObj)
+print("list of dict turn to json",listObj,"->",jsonData)
+print("data type of json array is",type(jsonData),"in python")
+print(type(listObj[0]))
