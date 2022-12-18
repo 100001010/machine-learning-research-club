7037c2795f384c59abf7619948ce7cd515428084
100001010 <119588517+100001010@users.noreply.github.com>
2022-12-18 16:35:07 +0800

    add web srawler 1_5~1_8

---
+++ learing/wab crawler/json type/1_8 python turn to json file.py
@@ -0,0 +1,8 @@
+import json
+
+dictObj ={"s":"20","sb":"87"}
+
+fn = 'out1_8.json'                  #file name
+with open(fn,'w') as fnObj:
+    json.dump(dictObj,fnObj)        #anyother write : fnobj = json.dump(dictObj) ***no dumps
+    
