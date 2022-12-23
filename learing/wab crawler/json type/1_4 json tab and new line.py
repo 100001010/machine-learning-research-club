9a95dd04008f99841f1ea56356ae16084c87987e
100001010 <119588517+100001010@users.noreply.github.com>
2022-12-23 16:36:27 +0800

    1

--- learing/wab crawler/json type/1_4 json tab and new line.py
+++ learing/wab crawler/json type/1_4 json tab and new line.py
@@ -5,6 +5,7 @@ players={'Stephen Curry':'Golden State Warriors',
          'James Harden':'Houston Rockets'
         }
 
-jsonObj = json.dumps(players,sort_keys=True,indent=4)
-jsonObj = json.dumps(players,indent=4,sort_keys=True)
-print (jsonObj)
+jsonObj1 = json.dumps(players,sort_keys=True,indent=4)
+jsonObj2 = json.dumps(players,indent=4,sort_keys=True)
+print (jsonObj1)
+print (jsonObj1!=jsonObj2)
