b5dcd5785d44c369471fde87d6fbf4215a5dae82
eric <eric93522@gmail.com>
2023-03-17 09:59:33 +0800

    f

--- Technical_analysis/MACD.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('/Users/apple/Documents/github/machine learning research club/Technical_analysis/test.json') as fnobj:
-    data = json.load(fnobj)
-
-for i in range(len(data["ta"])):
-    print(data["ta"][i]["v"])
-
-# EMA
---
+++ Technical_analysis/bastcin_formation/candlestick/1d.py
@@ -0,0 +1,8 @@
+import json
+
+with open('/data/1210.json') as f:
+    data = json.load(f)
+
+fori in range(-270,0)
+
+
