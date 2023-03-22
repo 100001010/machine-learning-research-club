256239c15dc6fd251180346caa1a6019d221780d
eric <eric93522@gmail.com>
2023-03-22 11:27:31 +0800

    up

--- Technical_analysis/1d.py
+++ Technical_analysis/1d.py
@@ -1,7 +1,7 @@
 import json
 import pandas
 
-with open('/data/1101.json') as f:
+with open('1101.json') as f:
     temp_data = json.load(f)
 m1 = temp_data["basicin_formation"]["candlestick"]["1m"] 
 
@@ -10,10 +10,6 @@ min =200000
 for i in range(-270,0):
     if m1[i]["h"]>max:
         max = m1[i]["h"]
-    if m1[i]["l"]>max:
-        max = m1[i]["h"]
+    if m1[i]["l"]<min:
+        min = m1[i]["h"]
     
-        
-    
-print("iiiiiiiiiiiiiiiiＮ")
-
